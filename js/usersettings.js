3b4b6498352956470f414a45ffb93a940ff4f6ec
Pedro Couceiro <202200701@estudantes.ips.pt>
2024-01-25 19:01:36 +0000

    Prompt added, css adjustments

--- js/usersettings.js
+++ js/usersettings.js
@@ -1,24 +1,51 @@
-let profile =  
-{ 
-    name:"user1", 
-    password:"password1"
+let profile =
+{
+    name: "user1",
+    password: "password1"
 }
 
 
-function init()
-{
+function init() {
     console.log("init");
+
+    getCurrentTime();
 
     let profileLocalStorage = localStorage.getItem("UserProfile");
 
     let profileDicionario = JSON.parse(profileLocalStorage);
 
+    document.getElementById("txt_username").textContent = profileDicionario.name;
+
     document.getElementById("txt_nome").value = profileDicionario.name;
-    document.getElementById("txt_pass").value = profileDicionario.pass;    
+    document.getElementById("txt_pass").value = profileDicionario.pass;
 }
 
-function changeUser()
-{
+function getCurrentTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+
+    if (hh == 0) {
+        hh = 12;
+    }
+    if (hh > 12) {
+        hh = hh - 12;
+        session = "PM";
+    }
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + " " + session;
+
+    document.getElementById("HorasSpan").textContent = time;
+    setTimeout(function () { getCurrentTime() }, 1000);
+}
+
+function changeUser() {
     let txt_username = document.getElementById("txt_nome").value;
     let txt_pass = document.getElementById("txt_pass").value;
 
